118c8c9dca87f040ae6ac1e36e14cd296a775740
QuanTran <tnminhquan2104@gmail.com>
2025-09-28 11:09:09 +0700

    Initial commit: Hello World with Copilot

--- main.cpp
+++ main.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
+
 using namespace std;
-int main()
-{
-  float grade1 = 85.5; 
-  float grade2 = 97.0;
-  float total, average;
-   total = grade1 + grade2;
-  average = total/2.0;  // divide the total by 2.0
-  cout << "The average grade is " << average << endl;
-  return 0;
+
+int main(){
+    cout << "Hello, World!" << endl;
+    return 0;
 }
